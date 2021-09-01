2f2e55d70d99ed0b0c851ddaecd27d9892dd1145
Raul Castro <raulc27@gmail.com>
2021-09-01 01:50:58 -0300

    ultimas modificacoes, exclusao de item etc

--- genero/urls.py
+++ genero/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.cadastro,name='cadastro')
+    path('',views.cadastro,name='cadastro'),
+    path('delete/<id>',views.delete,name='delete'),
 ]
--- genero/views.py
+++ genero/views.py
@@ -1,6 +1,8 @@
+from django.http.response import HttpResponseNotFound
 from django.shortcuts import render
+from django.http import HttpResponseNotAllowed
 from . import forms
-
+from . import models
 # Create your views here.
 
 def cadastro(request):
@@ -12,5 +14,16 @@ def cadastro(request):
             form.save(commit=True)
         else:
             print("ERROR")
-    data_dict={'form':form}
+    generos_list = models.Genero.objects.order_by('descricao');
+    data_dict={'form':form,'generos_records':generos_list}
     return render(request,'genero/genero.html',data_dict)
+
+def delete(request,id):
+    try:
+        models.Genero.objects.filter(id=id).delete()
+        form = forms.GeneroForm();
+        generos_list = models.Genero.objects.order_by('descricao')
+        data_dict={'form':form,'generos_records':generos_list}
+        return render(request,'genero/genero.html',data_dict)
+    except:
+        return HttpResponseNotAllowed()
