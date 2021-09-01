8fa622ae09890c63acf4f1b908ad8ab8069493e3
Raul Castro <raulc27@gmail.com>
2021-09-01 00:31:33 -0300

    mudando paradigma, usando models...

--- genero/forms.py
+++ genero/forms.py
@@ -1,7 +1,12 @@
 from django import forms
+from genero.models import Genero
 class GeneroForm(forms.Form):
-    def __init__(self, *args, **kwargs):
-        super(GeneroForm, self).__init__(*args, **kwargs)
-        self.fields('descricao').error_messages={'required':'Campo Obrigatorio'}
+  #  def __init__(self, *args, **kwargs):
+  #      super(GeneroForm, self).__init__(*args, **kwargs)
+  #      self.fields('descricao').error_messages={'required':'Campo Obrigatorio'}
 
-    descricao = forms.CharField(label='Gênero', required=True)
+  #  descricao = forms.CharField(label='Gênero', required=True)
+
+    class Meta:
+        model = Genero
+        fields = '__all__'
