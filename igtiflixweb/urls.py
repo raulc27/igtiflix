b51f8d63c936ecfe8dce728f0bd0a53e57d1f511
Raul Castro <raulc27@gmail.com>
2021-09-02 09:23:41 -0300

    ultimas implementaçoes

--- igtiflixweb/urls.py
+++ igtiflixweb/urls.py
@@ -2,7 +2,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from .views import redirect_root
+
 urlpatterns = [
+    path('',redirect_root),
+    #path(route='',view=include('principal.urls')),
     path('admin/', admin.site.urls),
     path(route='principal/', view=include('principal.urls')),
     path(route='genero/',view=include('genero.urls')),
--- serie/models.py
+++ serie/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Serie(models.Model):
-    nome = models.CharField(max_length=150)
-    idGenero = models.ForeignKey('genero.Genero',on_delete=models.PROTECT)
+    nome = models.CharField('Título da Série',max_length=150)
+    idGenero = models.ForeignKey('genero.Genero',verbose_name='Gênero',on_delete=models.PROTECT)
     def __str__(self):
         return self.nome
